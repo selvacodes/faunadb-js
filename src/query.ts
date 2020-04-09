c5fb7574c25093e0368b4f0ef412dea1d9f52332
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-09 16:57:02 +0200

    Add collection and delete queries
    Maybe Refs should have nicer wrapper types...

--- src/query.ts
+++ src/query.ts
@@ -1,5 +1,28 @@
-import { CreateCollection } from './types';
+import { CollectionRef, CreateCollection, Delete } from './types';
 
 export const createCollection = (
   param_object: CreateCollection['create_collection']['object'],
 ): CreateCollection => ({ create_collection: { object: param_object } });
+
+/**
+ * The Collection function returns a valid Reference for the specified
+ * collection name, in the specified child database. If a child database
+ * is not specified, the returned collection reference belongs to the
+ * current database.
+ */
+export const collection = (name: string): CollectionRef => ({
+  '@ref': {
+    id: name,
+    collection: {
+      '@ref': { id: 'collections' },
+    },
+  },
+});
+
+/**
+ * https://docs.fauna.com/fauna/current/api/fql/functions/delete
+ * delete_ with underscore suffix, becase it's reverved word.
+ */
+export const delete_ = (ref: CollectionRef): Delete => ({
+  delete: { collection: ref['@ref'].id },
+});
