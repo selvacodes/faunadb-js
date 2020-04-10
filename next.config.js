a3c6ed1718a533ecba8c7e682d0b10a18b22bfdf
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-10 22:19:43 +0200

    Delete next.config.js, not necessary anymore

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack(config, options) {
-    // Enable tree shaking.
-    // https://github.com/gcanti/fp-ts/issues/1044#issue-536939300
-    config.resolve.alias['fp-ts/lib'] = 'fp-ts/es6';
-    config.resolve.alias['io-ts/lib'] = 'io-ts/es6';
-    return config;
-  },
-};
