1b8089907b67c019bab7317dbde7d6f612d834ed
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-10 02:56:25 +0200

    Add full export * as example

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import * as types from './types';
 
 export { Client, query, types };
 
-// For some reason, `export * as query from './query';` does not work with Next.js.
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#-export--as-ns-syntax
-// https://github.com/zeit/next.js/issues/11762
+// // For some reason, `export * as` does not work with Next.js.
+// // It's probably because of target.
+// // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#-export--as-ns-syntax
+// // https://github.com/zeit/next.js/issues/11762
+// export * from './Client';
+// export * as query from './query';
+// export * as types from './types';
