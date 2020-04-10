ab92a9c8aaf5fcfb64e04ba392d055a0fa32384b
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-11 00:32:21 +0200

    Reenable tree-shaking
    https://github.com/gcanti/fp-ts/issues/1044#issuecomment-612223377

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// Once Next.js will use Webpack 5, this will not be necessary.
+module.exports = {
+  webpack(config, options) {
+    // Enable tree shaking.
+    // https://github.com/gcanti/fp-ts/issues/1044#issue-536939300
+    config.resolve.alias['fp-ts/lib'] = 'fp-ts/es6';
+    config.resolve.alias['io-ts/lib'] = 'io-ts/es6';
+    return config;
+  },
+};
