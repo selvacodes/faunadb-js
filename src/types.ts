e4e906e3a9e5fe6426a40d73deb5e83da1e5670d
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-10 02:55:28 +0200

    Refactor RequestResponse, add FaunaFetchError, FaunaHttpError stub, FaunaError

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,74 @@ import * as E from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 
 // Notes:
-//  - We don't use branded types (like FaunaID, Days, etc.) for Fauna API,
-//    because it would confuse people. To be reconsider later.
+//  - All types are io-ts types, so we can serialize/deserialize them safely.
+//  - We don't use branded types (like FaunaID, Days, etc.),
+//    because it would confuse people. To be reconsidered later.
 //  - We don't use t.strict, because it only removes props.
+
+// Errors
+
+/**
+ * Fetch error. It can happen when network is down.
+ */
+export const FaunaFetchError = t.type({
+  type: t.literal('FaunaFetchError'),
+  message: t.string,
+});
+export type FaunaFetchError = t.TypeOf<typeof FaunaFetchError>;
+
+/**
+ * Errors returned by the FaunaDB server.
+ */
+export const FaunaHttpError = t.type({
+  type: t.literal('FaunaHttpError'),
+  // TODO
+});
+export type FaunaHttpError = t.TypeOf<typeof FaunaHttpError>;
+
+export const FaunaError = t.union([FaunaFetchError, FaunaHttpError]);
+export type FaunaError = t.TypeOf<typeof FaunaError>;
+
+// Models
+
+/**
+ * A structure containing the request and response context for a given FaunaDB
+ * request. Provided to an observer function optionally defined in the
+ * `Client` constructor.
+ */
+export const RequestResponse = t.type({
+  /** The HTTP method used in the request. */
+  method: t.keyof({ GET: null, POST: null }),
+
+  /** The path that was queried. Relative to the client's domain. */
+  path: t.string,
+
+  /** The HTTP response status code. */
+  statusCode: t.number,
+
+  /** The HTTP headers returned in the response. */
+  responseHeaders: t.UnknownRecord,
+
+  /** The time the request was issued by the client. */
+  startTime: t.number,
+
+  /** The time the response was received by the client. */
+  endTime: t.number,
+
+  /** `this.endTime - this.startTime`: Time taken in milliseconds. */
+  timeTaken: t.number,
+
+  // TODO:
+  // /** URL query parameters. Only set if `method` is "GET". */
+  // query: t.union([t.string, t.null]),
+
+  /** The JSON request string, if any. */
+  request: t.union([t.string, t.undefined]),
+
+  /** The response data parsed as JSON. */
+  response: t.unknown,
+});
+export type RequestResponse = t.TypeOf<typeof RequestResponse>;
 
 /**
  * Fauna Long type as JavaScript Date.
@@ -20,44 +85,6 @@ const DateFromFaunaLong = new t.Type<Date, number, unknown>(
     }),
   (a) => a.getTime() * 1000,
 );
-
-/**
- * A structure containing the request and response context for a given FaunaDB
- * request. Provided to an observer function optionally defined in the
- * `Client` constructor.
- */
-export const RequestResult = t.type({
-  /** The HTTP method used in the request. */
-  method: t.keyof({
-    GET: null,
-    POST: null,
-    PUT: null,
-    PATCH: null,
-    DELETE: null,
-  }),
-  /** The path that was queried. Relative to the client's domain. */
-  path: t.string,
-  /** URL query parameters. Only set if `method` is "GET". */
-  query: t.union([t.string, t.null]),
-  /** The JSON request string. */
-  requestRaw: t.string,
-  /** The request data. */
-  requestContent: t.UnknownRecord,
-  /** The unparsed response data, as a string. */
-  responseRaw: t.string,
-  /** The response data parsed as JSON. */
-  responseContent: t.UnknownRecord,
-  /** The HTTP response status code. */
-  statusCode: t.number,
-  /** The HTTP headers returned in the response. */
-  responseHeaders: t.UnknownRecord,
-  /** The time the request was issued by the client. */
-  startTime: t.number,
-  /** The time the response was received by the client. */
-  endTime: t.number,
-});
-
-export type RequestResult = t.TypeOf<typeof RequestResult>;
 
 /**
  * The CreateCollection function is used to create a collection that groups
