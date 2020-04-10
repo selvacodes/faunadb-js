8357c8571b7262d5a83224e1cae2405be89791ea
Daniel Steigerwald <daniel@steigerwald.cz>
2020-04-10 21:02:59 +0200

    Do not use io-ts-types because it increases the number of peer dependencies
    And we don't need it, we will copy-paste what we will need.

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
     // https://github.com/gcanti/fp-ts/issues/1044#issue-536939300
     config.resolve.alias['fp-ts/lib'] = 'fp-ts/es6';
     config.resolve.alias['io-ts/lib'] = 'io-ts/es6';
-    config.resolve.alias['io-ts-types/lib'] = 'io-ts-types/es6';
-    config.resolve.alias['monocle-ts/lib'] = 'monocle-ts/es6';
-    config.resolve.alias['newtype-ts/lib'] = 'newtype-ts/es6';
     return config;
   },
 };
